f92ec2683440bb554f7f6d6dc42f3eb21d45ad1c
chris <cl@startx.fr>
2016-10-29 12:28:42 +0200

    adding install script for configuring environement and installing sxapi-cli

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # sxapi-installer
 install sxapi working environement
+
+SXAPI Installer
+===============
+
+[![Travis](https://travis-ci.org//startxfr/sxapi-installer.svg?branch=master)](https://travis-ci.org/startxfr/sxapi-installer)
+
+***SXAPI*** is a microservice framework optimized for building simple and extensible API efficiently. 
+
+
+Getting Started
+---------------
+
+### Pre-requisites
+
+* Fedora Linux OS
+* sudo access
+* wget tool (```sudo yum install -y wget```)
+
+
+### Execute the installer
+
+Run the following commands
+
+```
+wget https://raw.githubusercontent.com/startxfr/sxapi-installer/master/install.sh -O - | bash
+```
+
+### Troubleshooting
+
+If you run into difficulties installing or running sxapi, please report [issue for installer](https://github.com/startxfr/sxapi-installer/issues/new) or  [issue for sxapi](https://github.com/startxfr/sxapi-core/issues/new).
+
+License
+-------
+
+SXAPI is licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/).
---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+function displayStartInstallation {
+    echo "" 
+    echo "==================================" 
+    echo "== SXAPI Installer on $(cat /etc/redhat-release)"
+    echo "==================================" 
+    echo ""
+}
+
+function checkRootAccess {
+    if [ "$(id -u)" != "0" ]; then
+        echo "! This script must be run as root"
+        echo "! Please run 'su root' and start again"
+        echo "! this installer"
+        exit;
+    else 
+        echo " - root access : OK"
+    fi
+}
+
+function checkDependency {
+    PKGNAME=${1}
+    PKEXIST=$(rpm -qa | grep -c $PKGNAME)
+    if [ $PKEXIST == "0" ]; 
+    then
+        echo " - $PKGNAME : NOT FOUND"
+        echo -n "   Installing $PKGNAME ... "
+        yum install -y $PKGNAME &> /dev/null
+        echo "DONE"
+    else 
+        echo " - $PKGNAME : FOUND"
+    fi
+}
+
+function checkDockerCompose {
+    VERSION="1.8.0"
+    if [ ! -f /usr/local/bin/docker-compose ]; then
+        echo " - docker-compose : NOT FOUND"
+        echo -n "   Installing docker-compose ... "
+        curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+        chmod +x /usr/local/bin/docker-compose
+        echo "DONE"
+    else 
+        echo " - docker-compose : FOUND"
+    fi
+}
+
+function checkSxapiCli {
+    VERSION="master"
+    if [ ! -f /usr/local/bin/sxapi-installer ]; then
+        echo " - sxapi-cli : NOT FOUND"
+        echo -n "   Installing sxapi-cli ... "
+        curl -L https://github.com/startxfr/sxapi-console/releases/download/$VERSION/cli.sh > /usr/local/bin/sxapi-cli
+        chmod +x /usr/local/bin/sxapi-cli
+        echo "DONE"
+    else 
+        echo " - sxapi-installer : FOUND"
+    fi
+}
+
+function displayEndInstallation {
+    echo ""
+    echo "   sxapi-installer"
+    echo "   microservices build on top of sxapi."
+    echo "   to see how it work, please visit http://github.com/startxfr/sxapi-sample"
+    echo "   or you can try by running"
+    echo "   # docker-compose up sample"
+    echo ""
+}
+
+displayStartInstallation
+checkRootAccess
+checkDependency "git"
+checkDependency "curl"
+checkDependency "wget"
+checkDependency "docker-io"
+checkDockerCompose
+checkSxapiCli
+displayEndInstallation
